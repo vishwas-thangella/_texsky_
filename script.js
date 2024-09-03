9bcedc54552a43cc1bdbf21af78227d9931f565b
Vishwas <vishwas56883669@gmail.com>
2024-09-03 21:36:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const SidebarOpen = () =>{
+    document.getElementById('sidebar').style.left = "0px";
+}
+
+const SidebarClose = () =>{
+    document.getElementById('sidebar').style.left = "-200px";
+}
